d19aac90e3beb7430ef69baf3d178f1e3b2b4f76
alexn <olek@wpgeeks.co.uk>
2018-12-15 09:35:34 +0100

    upadte 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dataJson from './data_json/test1.json';
+
+class JsonExample extends Component {
+    render() {
+        return (
+            <div>
+                <h3>{this.props.title}</h3>
+                <ul>
+                    {dataJson.map(function (obj, i) {
+
+                        if(i === 10) {
+                            return <li>
+                                {obj._pl} - {obj._en}
+                            </li>
+                        }
+
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
 
 class MainNavigation extends Component {
     render() {
@@ -9,10 +31,69 @@ class MainNavigation extends Component {
                 <h1><a href={"#"}><img src={logo} className="logo" alt="Flashcards" /> Flashcards</a></h1>
                 <h3>{this.props.title}</h3>
                 <ul>
-                    {this.props.items.map(i => <li><a href='#'>{i}</a></li>)}
+                    {/*{this.props.items.map(i => <li><a href='#'>{i}</a></li>)}*/}
+                    {this.props.items.map(function (obj, i) {
+                        return <li>
+                            <a href={"#id"+i}>{obj} - {i}</a>
+                        </li>;
+                    })}
                 </ul>
             </header>
         );
+    }
+}
+
+var data = [
+    {name:'Jhon', age:28, city:'HO'},
+    {name:'Onhj', age:82, city:'HN'},
+    {name:'Nohj', age:41, city:'IT'}
+];
+
+class ArraryExample extends Component {
+    render() {
+        return (
+            <div>
+                <h3>{this.props.title}</h3>
+                <ul>
+                    {data.map(function (obj, i) {
+                        return <li>
+                            <a href={"#id"+i}>{obj.name} - {obj.age} - {obj.city} - id: {i}</a>
+                        </li>;
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
+
+class OneToDo extends Component {
+    state = {
+        val: this.props.val,
+        draft: ''
+    };
+
+    updateText = event => {
+        this.setState({draft: event.target.value});
+    };
+
+    addText = () => {
+        // const { val, draft } = this.state;
+        this.setState({
+            val: this.state.draft,
+            draft: ''
+        });
+
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>{this.props.title}</h2>
+                <p>{this.state.val}</p>
+                <input type='text' placeholder='Change string...' onChange={this.updateText} value={this.state.draft} />
+                <button onClick={this.addText}>Change</button>
+            </div>
+        )
     }
 }
 
@@ -51,13 +132,56 @@ class ToDoList extends Component {
     }
 }
 
+class ExampleForm extends React.Component {
+    state = {
+        val: '...',
+        draft: ''
+    };
+
+    handleChange = (event) => {
+        this.setState({draft: event.target.value});
+    }
+
+    handleSubmit = (event) => {
+        this.setState({
+            val: this.state.draft,
+            draft: ''
+        });
+        // const { val, draft } = this.state;
+        // this.setState(prevState => ({
+        //     val: [...prevState.value, draft],
+        //     draft: ''
+        // }));
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h2>{this.props.title}</h2>
+                <div className="show">{this.state.val}</div>
+                <input placeholder={this.state.placeholder} onChange={this.handleChange} value={this.state.draft} />
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+/**
+ * all
+ */
+
 class App extends Component {
 
     render() {
         return (
             <div>
-                <MainNavigation title='Navigation' items={['PL -> EN','EN -> PL']} />
-                <ToDoList title='TodoList' tasks={['item1','item2','item3']} />
+                <MainNavigation title='Class MainNavigation' items={['PL -> EN','EN -> PL']} />
+                <ToDoList title='Class ToDoLis' tasks={['item1','item2','item3']} />
+                <OneToDo title='Class OneToDo' val="placeholder..." />
+                <ExampleForm title='Class ExampleForm' />
+                <ArraryExample title='Class ArraryExample' />
+                <JsonExample title='Class JsonExample' />
             </div>
         );
     }
